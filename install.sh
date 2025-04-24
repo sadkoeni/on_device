951b1e6ee7cd00b16b89a8ce1c2f65546c98ff5e
sadkoeni <stephan.koenigstorfer@gmail.com>
2025-04-25 00:34:45 +0400

    added bt wifi setup tool install

--- install.sh
+++ install.sh
@@ -17,10 +17,28 @@ cd lb
 
 echo "installing startup services"
 sudo cp lightberry.service /etc/systemd/system/ || { echo "Failed to copy lightberry.service. Exiting."; exit 1; }
-sudo cp managment/bt-wifi-config.service /etc/systemd/system/ || { echo "Failed to copy bt-wifi-config.service. Exiting."; exit 1; }
+sudo cp btwifiset.service /etc/systemd/system/ || { echo "Failed to copy bt-wifi-config.service. Exiting."; exit 1; }
 sudo cp gitpull.service /etc/systemd/system/ || { echo "Failed to copy gitpull.service. Exiting."; exit 1; }
 
+echo "installing bt wifi setup"
+sudo apt install -y python3 python3-pip bluetooth bluez python3-dbus || {echo "Failed to install btwifi setup dependencies. Exiting."; exit 1;}
+
+cd /usr/local/
+echo "downloading bt-wifi setup"
+sudo git clone https://github.com/mkaczanowski/btwifiset.git || {echo "Failed to download btwifi setup git repo. Exiting."; exit 1;}
+
+cd btwifiset
+sudo pip3 install -r requirements.txt
+
+
 echo "enabling services so they start on system boot"
+
+sudo systemctl daemon-reload
+
 sudo systemctl enable lightberry || { echo "Failed to enable lightberry. Exiting."; exit 1; }
-sudo systemctl enable bt-wifi-config || { echo "Failed to enable bt-wifi-config. Exiting."; exit 1; }
+sudo systemctl enable btwifiset.service|| { echo "Failed to enable bt-wifi-config. Exiting."; exit 1; }
 sudo systemctl enable gitpull || { echo "Failed to enable gitpull. Exiting."; exit 1; }
+
+sudo systemctl start btwifiset.service
+
+cd ~/lb
